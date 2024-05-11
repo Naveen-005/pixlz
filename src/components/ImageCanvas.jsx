4a8078f1727b5d83bc3584b81fa30998f3d783e9
Naveen-005 <naveenalex512@gmail.com>
2024-05-11 18:47:49 +0530

    replaced img with canvas

--- src/components/ImageCanvas.jsx
+++ src/components/ImageCanvas.jsx
@@ -1,15 +1,40 @@
-import { useRef,useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import './ImageCanvas.css'
+//import Jimp from "jimp";
 
 const ImageCanvas = (props) => {
+
+  const canvasRef = useRef(null);
+
+  const img=new Image()
+
+  useEffect(()=>{
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+
+    img.src=URL.createObjectURL(props.image)
+
+  
+  },[props.image])
   
 
+
+
   return (
-
-<div className="image-container">
-  <img src={URL.createObjectURL(props.image)} className="img-fluid" alt="" />
-</div>
-
+    <div className="image-container">
+      {/*<img src={URL.createObjectURL(props.image)} className="img-fluid" alt="" />*/}
+      <canvas ref={canvasRef} className="img-fluid"/>
+    </div>
   );
 };
 
