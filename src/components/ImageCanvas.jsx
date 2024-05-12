0b9b79ef271998510cddfebaf85d9a7511e891d0
Naveen-005 <naveenalex512@gmail.com>
2024-05-12 16:16:11 +0530

    small bugs

--- src/components/ImageCanvas.jsx
+++ src/components/ImageCanvas.jsx
@@ -1,60 +1,45 @@
-import React, { useRef, useState, useEffect } from "react";
-import './ImageCanvas.css'
-//import Jimp from "jimp";
+import React, { useRef, useEffect } from "react";
 import { brightnessChange } from "../scripts/adjustments";
+import './ImageCanvas.css';
 
 const ImageCanvas = (props) => {
-
   const canvasRef = useRef(null);
-  const[ctx,setCtx]=useState(null)
-  const[canvas,setCanvas]=useState(null)
 
-  const img=new Image()
-  //let canvas,ctx
-  //console.log("Outside brightness: ",props._adjustments.brightness)
-  //console.log("props: ",props)
-
-  useEffect(()=>{
-
-    setCanvas(canvasRef.current);
-    setCtx(canvas?.getContext('2d'))
-
-    ctx?.clearRect(0, 0, canvas.width, canvas.height);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
     
+    const img = new Image();
     img.onload = () => {
-
-      canvas.width = img?.width;
-      canvas.height = img?.height;
-      ctx?.drawImage(img, 0, 0);
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
     };
+    img.src = URL.createObjectURL(props.image);
 
-    img.src=URL.createObjectURL(props.image)
+    return () => {
+      URL.revokeObjectURL(img.src);
+    };
+  }, [props.image]);
 
-  
-  },[props.image])
+  const brightnessFirstRender=useRef(true);
+  useEffect(() => {
 
-  useEffect(()=>{
-
-    console.log("brightness: ",props._adjustments.brightness)
-
-    var imageData = ctx?.getImageData(0, 0, canvas.width, canvas.height);
-
-    if(imageData){
-      
-      imageData=brightnessChange(imageData,props._adjustments.brightness);
-      //console.log("image data:",imageData)
-      ctx.putImageData(imageData, 0, 0);
+    if(brightnessFirstRender.current){
+      brightnessFirstRender.current=false
+      return
     }
 
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const adjustedData = brightnessChange(imageData, props._adjustments.brightness);
+    ctx.putImageData(adjustedData, 0, 0);
 
-    //img.src = canvas.toDataURL();
-
-  },[props._adjustments.brightness])
-  
+  }, [props._adjustments.brightness]);
 
   return (
     <div className="image-container">
-      {/*<img src={URL.createObjectURL(props.image)} className="img-fluid" alt="" />*/}
       <canvas ref={canvasRef} className="img-fluid"/>
     </div>
   );
