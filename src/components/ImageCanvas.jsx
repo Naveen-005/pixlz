2a897ca14e9fddc202dd6af827643af00d16f1e2
Naveen-005 <naveenalex512@gmail.com>
2024-05-12 16:44:53 +0530

    brightness working

--- src/components/ImageCanvas.jsx
+++ src/components/ImageCanvas.jsx
@@ -4,16 +4,25 @@ import './ImageCanvas.css';
 
 const ImageCanvas = (props) => {
   const canvasRef = useRef(null);
+  const brightnessLayerRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+
+    
     
     const img = new Image();
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
+/*
+      if(props._adjustments.brightness!==0){
+        
+      }
+*/
+      
     };
     img.src = URL.createObjectURL(props.image);
 
@@ -32,15 +41,27 @@ const ImageCanvas = (props) => {
 
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    const brightnessLayerCanvas = brightnessLayerRef.current;
+    const brightnessLayerCtx = brightnessLayerCanvas.getContext('2d');
+    brightnessLayerCanvas.width=canvas.width;
+    brightnessLayerCanvas.height=canvas.height;
+    brightnessLayerCtx.clearRect(0, 0, brightnessLayerCanvas.width, brightnessLayerCanvas.height);
+    brightnessLayerCtx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    brightnessLayerCtx.fillRect(0, 0, brightnessLayerCanvas.width, brightnessLayerCanvas.height);
+
+
+
+    const imageData = brightnessLayerCtx.getImageData(0, 0, canvas.width, canvas.height);
     const adjustedData = brightnessChange(imageData, props._adjustments.brightness);
-    ctx.putImageData(adjustedData, 0, 0);
+    brightnessLayerCtx.putImageData(adjustedData, 0, 0);
 
   }, [props._adjustments.brightness]);
 
   return (
     <div className="image-container">
       <canvas ref={canvasRef} className="img-fluid"/>
+      <canvas ref={brightnessLayerRef} className="img-fluid" style={{ position: 'absolute', top: 0, left: 0 }}/>
     </div>
   );
 };
